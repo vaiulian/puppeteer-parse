20698e297b7a5a816452f8ae61e2ca1dd7631593
vaiulian <vasileaiulian@gmail.com>
2019-07-02 00:15:37 +0300

    refactor for second page

--- index.js
+++ index.js
@@ -1,83 +1,104 @@
-
-const puppeteer = require('puppeteer');
-const download = require('image-downloader');
-const shortid = require('shortid');
-
-console.log('1 ',shortid.generate());
-
+const puppeteer = require("puppeteer");
+const download = require("image-downloader");
+const sss = require("shortid");
 
 let scrape = async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+
     const page = await browser.newPage();
 
-    await page.goto('https://www.imdb.com/search/name?gender=male');
+    const result = [];
+
+    await page.goto("https://www.imdb.com/search/name?gender=male");
     await page.waitFor(1000);
-    console.log('2 ',shortid.generate());
 
-    const result = await page.evaluate(async(shortid) => {
+    const result1 = await getFromPage(page);
+    await loopActorsInList(result1.slice(0, 1), result1);
+    result.push(result1);
 
-        console.log('ceva ', shortid);
 
-        console.log('3 ',shortid.generate());
+    await page.goto("https://www.imdb.com/search/name/?gender=male&start=51&ref_=rlm");
+    await page.waitFor(1000);
 
-        let data = []; // Create an empty array
-        let elements = document.querySelectorAll('.lister-item.mode-detail');
+    const result2 = await getFromPage(page);
+    await loopActorsInList(result2.slice(0, 1), result2);
+    result.push(result2);
 
-        Array.from(elements).map((item) => {
-            // get name and link from every item
-            let name = item.querySelector('.lister-item-header a').innerText;
-            let link = item.querySelector('.lister-item-header a').href;
-
-            data.push({name, link}); // Push the data to our array
-
-        })
-
-        return data;
-    }, shortid);
-
-    let abc = result.slice(0,4);
-
-    await loopActorsInList();
     await browser.close();
-    return result;
 
+    console.log(result1);
+    console.log(result2);
 
-    async function loopActorsInList() {
+    return [...result1, ...result2];
+
+    async function getFromPage(page) {
+        return await page.evaluate(async () => {
+            let data = []; // Create an empty array
+            let elements = document.querySelectorAll(".lister-item.mode-detail");
+
+            Array.from(elements).map(item => {
+                // get name and link from every item
+                let name = item.querySelector(".lister-item-header a").innerText;
+                let link = item.querySelector(".lister-item-header a").href;
+
+                data.push({
+                    name,
+                    link
+                });
+            });
+
+            return data;
+        });
+    }
+
+    async function loopActorsInList(abc, result) {
         // sync for loop
         for (let i = 0; i < abc.length; i++) {
-            console.log('Page ID Spawned', i)
+            console.log("Page ID Spawned", i);
             //async promise
             await new Promise(async resolve => {
-                let actorPage = await browser.newPage()
+                let actorPage = await browser.newPage();
                 await actorPage.goto(abc[i].link); //
-                    
+
                 const linkImage = await actorPage.evaluate(() => {
-                    let image = document.querySelector('#name-poster').src
+                    let image = document.querySelector("#name-poster").src;
                     return image;
                 });
 
                 result[i].linkImage = linkImage;
+                result[i].id = result[i].link.split('/').pop();
                 await actorPage.close();
                 await downloadIMG(result[i]);
-                
-                resolve()
+
+                resolve();
             });
         }
     }
 
     async function downloadIMG(item) {
         try {
-            const { linkImage, name } = item;
-            const path = name + ".jpg"
-            const { filename, image } = await download.image({url: linkImage, dest: path})
-            console.log(filename) // => /path/to/dest/image.jpg 
+            const {
+                id,
+                linkImage,
+                name
+            } = item;
+            const path = `${id}-${name.split(' ')[0]}.jpg`;
+            const {
+                filename
+            } = await download.image({
+                url: linkImage,
+                dest: path
+            });
+            console.log(filename); // => /path/to/dest/image.jpg
         } catch (e) {
-            console.error(e)
+            console.error(e);
         }
     }
 };
 
-scrape().then((value) => {
-    console.log('start')
+scrape().then(value => {
+    console.log("start");
     console.log(value); // Success!
 });
